c2aeb9305dc2b688ad0adfc1299077f5a5bf8734
TchamiDev <tchamistephane@yahoo.fr>
2023-08-18 19:05:20 +0200

    charging pic on the website

--- ec/app/models.py
+++ ec/app/models.py
@@ -146,3 +146,38 @@ class Cart(models.Model):
     @property
     def total_cost(self):
         return self.quantity * self.product.discounted_price
+
+
+STATUS_CHOICES = (
+    ("Accepté", "Accepté"),
+    ("Emballé", "Emballé"),
+    ("En chemin", "En chemin"),
+    ("Livré", "Livré"),
+    ("Annuler", "Annuler"),
+    ("En attente", "En attente"),
+)
+
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    stripe_order_id = models.CharField(max_length=100, blank=True, null=True)
+    stripe_payment_status = models.CharField(max_length=100, blank=True, null=True)
+    stripe_payment_id = models.CharField(max_length=100, blank=True, null=True)
+    paid = models.BooleanField(default=False)
+
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=57, choices=STATE_CHOICES, default="En attente"
+    )
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, default="")
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
